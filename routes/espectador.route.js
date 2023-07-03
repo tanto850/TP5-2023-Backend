7a5479afeceaf6e556e23aca13e5995a42be76e5
Antonela Flores <totte_30_98@hotmail.com>
2023-07-03 17:30:31 -0300

    cambios en espectador

--- routes/espectador.route.js
+++ routes/espectador.route.js
@@ -1,5 +1,5 @@
 //defino controlador para el manejo de CRUD
-const espectadorCtrl = require('../controllers/espectador.controller');
+const espectadorCtrl = require('./../controllers/espectador.controller');
 //creamos el manejador de rutas
 const express = require('express');
 const router = express.Router();
@@ -7,7 +7,5 @@ const router = express.Router();
 router.get('/', espectadorCtrl.getEspectadores);
 router.post('/', espectadorCtrl.createEspectador);
 router.get('/:id', espectadorCtrl.getEspectador);
-router.put('/:id', espectadorCtrl.editEspectador);
-router.delete('/:id', espectadorCtrl.deleteEspectador);
 //exportamos el modulo de rutas
 module.exports = router;
